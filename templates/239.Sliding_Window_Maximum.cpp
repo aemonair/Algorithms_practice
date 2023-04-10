d622be6253201b57f0c5d4e04e69201df1cc9316
Aemonair <v.aemonair@gmail.com>
2023-04-10 21:54:39 +0800

    Sliding window test

--- templates/239.Sliding_Window_Maximum.cpp
+++ templates/239.Sliding_Window_Maximum.cpp
@@ -49,8 +49,11 @@
  ****************************************************************************** 
  */
 
+// 44 yy
+#include <unordered_map>
 #include <algorithm>
 #include <iostream>
+#include <climits>
 #include <chrono>
 #include <vector>
 #include <string>
@@ -60,30 +63,37 @@
 #include <map>
 #include <set>
  
-class MonotonicQueue
-{
-    std::list<int> q;
-public:
-    void push(int n)
-    {
-        while(!q.empty() && q.back() < n)
-        {
-            q.pop_back();
-        }
-        q.push_back(n);
-    }
-    int max()
-    {
-        return q.front();
-    }
-    void pop(int n)
-    {
-        if (n== max())
-        {
-            q.erase(q.begin());
-        }
-    }
-};
+//the following are UBUNTU/LINUX ONLY terminal color codes.
+#define     RESET   "\033[0m"
+#define     RED     "\033[31m"             /*      Red     */
+#define     CYAN    "\033[36m"             /*      Cyan    */
+#define     BLUE    "\033[34m"             /*      Blue    */
+#define     GREEN   "\033[32m"             /*      Green   */
+#define     WHITE   "\033[37m"             /*      White   */
+#define     BLACK   "\033[30m"             /*      Black   */
+#define     YELLOW  "\033[33m"             /*      Yellow  */
+#define     MAGENTA "\033[35m"             /*      Magenta */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow  */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black   */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White   */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green   */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue    */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan    */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red     */
+
+template<typename T>
+std::ostream & operator << (std::ostream &out, std::vector<T> &_vec);
+
+template<typename T>
+int printvector(std::vector<T> v);
+
+template <typename T>
+int printstack(std::stack<T> s);
+
+template <typename T1, typename T2>
+int printunordered_map(const std::unordered_map<T1,T2> &v);
+ 
 class Solution 
 {
 public:
@@ -92,50 +102,9 @@ public:
             std::vector<int>& nums, int k
             )
     {
+        return {};
         std::vector<int> ans;
         return ans;
-    }
-    template <typename T>
-    int printstack(std::stack<T> s)
-    {
-        if (s.empty())
-        {
-            std::cout << "The stack is empty." << std::endl;
-            return 0;
-        }
-        std::cout <<  "The stack size is: " << s.size() << std::endl;
-        std::stack<T> tmp;
-        while (!s.empty())
-        {
-            std::cout << s.top() << ", ";
-            s.pop();
-        }
-        std::cout <<  std::endl;
-        return s.size();
-    }
-
-    template <typename T>
-    int printvector(const std::vector<T> &v)
-    {
-        std::cout << "vector size: " << v.size() << std::endl;
-        for (auto iter = v.begin(); iter != v.end(); iter++ )
-        {
-            std::cout << *iter << "| ";//<<std::endl;
-        }
-        std::cout << std::endl;
-        return v.size();
-    }
-
-    template <typename T>
-    int printvectorvector(const std::vector<T> &v)
-    {
-        std::cout << "this vector size: " << v.size() << std::endl;
-        for (auto iter = v.begin(); iter != v.end(); iter++ )
-        {
-            printvector( *iter );
-        }
-        std::cout << std::endl;
-        return v.size();
     }
 };
 
@@ -146,7 +115,9 @@ void Test(const std::string& testName,
         )
 {
     if(testName.length() > 0)
-        std::cout <<testName << " begins: "<< std::endl;
+    {
+        std::cout << BOLDMAGENTA << testName << " begins: "<< RESET << std::endl;        
+    }
 
     Solution solution;
 
@@ -154,27 +125,119 @@ void Test(const std::string& testName,
     decltype(start) end ;
     auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
-    // getpermutataion
+    std::cout << "k:" << k << ", nums:" << std::endl;
+    printvector(nums);
+
+const static int TEST_TIME = 1;
+const static int TEST_0    = 1;
+const static int TEST_1    = 0;
+    if (TEST_0)
     {
-        //start = std::chrono::system_clock::now();
+        std::cout << "Solution0 start.........." << std::endl;
+        if (TEST_TIME)
+        {
+            start = std::chrono::system_clock::now();
+        }
 
-        std::cout << "nums:" << std::endl;
-        solution.printvector(nums);
-
-        //decltype(expected) 
         std::vector<int> && result = solution.maxSlidingWindow(nums, k);
-        std::cout << "result:" << std::endl;
-        solution.printvector(result);
+        //decltype(expected) result = solution.longestPalindrome(s);
+        std::cout << "result:" << std::boolalpha << result <<  std::endl;
 
         if(result == expected)
-            std::cout << "Solution0 passed." << std::endl;
+        {
+            //10yy
+            std::cout << GREEN << "Solution0 passed." << RESET <<  std::endl;
+        }
         else
-            std::cout << "Solution0 failed." << std::endl;
-        //end = std::chrono::system_clock::now();
-        //elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        //std::cout << "Solution0 costs " << elapsed.count() <<"micros" << std::endl;
+        {
+            std::cout << RED << "Solution0 failed." <<  RESET << std::endl;
+            std::cout << RED << "expected:" << expected << std::endl;
+            std::cout << RESET << std::endl;
+        }
+        if (TEST_TIME)
+        {
+           end = std::chrono::system_clock::now();
+           elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+           std::cout << "Solution0 costs " << elapsed.count() <<"micros" << std::endl;
+        }
     }
 }
+// 76 yy
+template<typename T>
+std::ostream & operator << (std::ostream &out, std::vector<T> &_vec)
+{
+    out << "[  ";
+    for(auto v: _vec)
+    {
+        out << v << ", ";
+    }
+    out << "\b\b ]" ;
+    return out;
+}
+template<typename T>
+int printvector(std::vector<T> v)
+{
+    if(0 == v.size())
+    {
+        std::cout << "Empty vector." << std::endl;
+        return 0;
+    }
+    std::cout << "[ " ;
+    for(auto i: v)
+    {
+        std::cout << i << ", ";
+    }
+    std::cout << "\b\b ]" << std::endl;
+    return v.size();
+}
+template<typename T>
+int printstack (std::stack <T> s)
+{
+    if(s.empty())
+    {
+        std::cout << "Empty stack ." << std::endl;
+        return 0;
+    }
+    std::cout <<  "The stack size is: " << s.size() << std::endl;
+    std::cout << "[ " ;
+    while (!s.empty())
+    {
+        std::cout << s.top() << ", ";
+        s.pop();
+    }
+    std::cout << "\b\b ]" << std::endl;
+    return s.size();
+}
+template<typename T>
+int printvector(std::stack <T> s)
+{
+    if(s.empty())
+    {
+        std::cout << "Empty stack ." << std::endl;
+        return 0;
+    }
+    std::cout <<  "The stack size is: " << s.size() << std::endl;
+    std::cout << "[ " ;
+    while (!s.empty())
+    {
+        std::cout << s.top() << ", ";
+        s.pop();
+    }
+    std::cout << "\b\b ]" << std::endl;
+    return s.size();
+}
+template <typename T1, typename T2>
+int printunordered_map(const std::unordered_map<T1,T2> &v)
+{
+    std::cout << "unordered_map size: " << v.size() << std::endl;
+    for (auto iter = v.begin(); iter != v.end(); iter++ )
+    {
+        std::cout << "(" << iter->first << "," << iter->second<< "), ";//<<std::endl;
+    }
+    std::cout << std::endl;
+    return v.size();
+}
+
 void Test1()
 {
     std::vector<int> nums   = {1,3,-1,-3,5,3,6,7};
@@ -211,7 +274,6 @@ void Test5()
 
 int main()
 {
-    Solution solution;
 
     Test1();
     Test2();
